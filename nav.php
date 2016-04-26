b2e2edca0a1fc43bcca6af9c55d6cd58bf78ec70
JosephJamiel <jackal562@yahoo.com>
2016-04-26 19:36:37 -0400

    updated

--- nav.php
+++ nav.php
@@ -10,7 +10,6 @@
       </button>
       <a class="navbar-brand" href="/BRiley/index.php">Home</a>
     </div>
-
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
@@ -30,8 +29,8 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Samples <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="/BRiley/blogging.php">Blogging</a></li>
-            <li><a href="/BRiley/articles.php">Articles</a></li>
+            <li><a href="blogging.php">Blogging</a></li>
+            <li><a href="articles.php">Articles</a></li>
             <li><a href="">Something else here</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="#">Separated link</a></li>
